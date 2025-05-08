17e37221cd7c99d43ef9681b6cf674a705564503
taekwondodev <taekwondodev@outlook.it>
2025-05-08 13:12:07 +0200

    fix models

--- app/internal/models/user.go
+++ app/internal/models/user.go
@@ -7,12 +7,12 @@ import (
 )
 
 type User struct {
-	Sub          uuid.UUID `json:"sub" db:"sub"`
-	Username     string    `json:"username" db:"username"`
-	Email        string    `json:"email" db:"email"`
-	PasswordHash string    `json:"-" db:"password_hash"`
-	Role         string    `json:"role" db:"role"`
-	CreatedAt    time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
-	IsActive     bool      `json:"is_active" db:"is_active"`
+	Sub          uuid.UUID `json:"sub"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	IsActive     bool      `json:"is_active"`
 }
